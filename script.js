d1f7fcd0e7331e17d31e626059aff0e0007c8e09
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2019-11-25 12:50:01 -0600

    Added search bar in HTML, updated formatting, added pseudocode.

---
+++ script.js
@@ -0,0 +1,8 @@
+//User inputs information to search for city weather
+
+//weather info pops up on jumbotron
+
+//five day forecast appears beneath jumbotron
+
+//search history of cities appear below search bar
+
