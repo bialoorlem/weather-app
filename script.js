1aae015d51910b89271375f248e8c50d060837a7
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2019-11-25 15:28:17 -0600

    Added searchCity function. Continuing to work on it.

--- script.js
+++ script.js
@@ -9,5 +9,15 @@
 const searchCity = document.getElementById("searchCity");
 const cityInput = document.getElementById("cityInput");
 
-https://api.openweathermap.org/data/2.5/weather?q={city name}
+function findWeather(){
+
+    searchCity.addEventListener("click", function(){
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value + "&appid=52ea047875fb2135ea949a32ca9139be";
+        
+
+    })
+
+}
+
+
 
