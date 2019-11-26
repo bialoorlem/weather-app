fd322f6dac4f780c85bcf44585d8baa8f834782b
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2019-11-25 23:06:07 -0600

    Received help from Jake O'Toole. Inputted for loop and also created console log for five day forecast. Must customize to match variables and also be able to send out data to HTML.

--- script.js
+++ script.js
@@ -8,6 +8,8 @@
 
 //search history of cities appear below search bar
 
+storageData = window.localStorage;
+
 const searchCity = document.getElementById("searchCity");
 const cityInput = document.getElementById("cityInput");
 
@@ -17,17 +19,27 @@ const cityInput = document.getElementById("cityInput");
 // const CURRENT_TEMP = document.getElementsByClassName('weather-content__temp')[0];
 // const FORECAST = document.getElementsByClassName('component__forecast-box')[0];
 
-//  $("#searchBtn").on("click", function(event){
+//  $("#searchCity").on("click", function(event){
 //         event.target.preventDefault;
 //         localStorage.setItem("city", city)
 //         localStorage.getItem("user")  
 
 
-function saveHistory(){
 
-    
+// function saveHistory(){
 
-}
+//   document.querySelectorAll(".btn").forEach(function(save){
+//     save.addEventListener("click", function(){
+//       let inputVal = save.getAttribute("value");
+
+//       console.log(inputVal);
+//     saveInput(inputVal);  
+
+//     })
+//   )}
+// }
+
+
 
 
 function findWeather(){
@@ -61,35 +73,65 @@ function findWeather(){
         
         
 
+
     });
 
     function fiveDayForecast(){
 
     searchCity.addEventListener("click", function(event){
 
-        let fiveDay = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityInput.value + "&appid=0eb7a574ca817a0762b55aa593d91036";
+        let fiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput.value + "&appid=0eb7a574ca817a0762b55aa593d91036";
         event.preventDefault();
+
+
 
         console.log(fiveDay);
 
         axios.get(fiveDay)
         .then(function(response){
 
+//Taken from Jake OToole
+
+let fiveDayWeather = [];
+
+
+for(let i=0; i < response.data.list.length; i++){
+                const timeUNIX = response.data.list[i].dt;
+                const stringTime = timeUNIX.toString();
+                const dateFormatted = moment(stringTime, "X").format("MM/DD/YYYY")
+                const timeFormatted = moment(stringTime, "X").format("HH:mm")
+                if(timeFormatted === "12:00"){
+                    const dayWeather = {
+                        date: dateFormatted,
+                        time: timeFormatted,
+                        weatherDescription: response.data.list[i].weather[0].description,
+                        weatherIcon: response.data.list[i].weather[0].icon,
+                        temp: response.data.list[i].main.temp,
+                        humidity: response.data.list[i].main.humidity
+                    }
+                    console.log("Day Weather: ", i, dayWeather)
+                    fiveDayWeather.push(dayWeather);
+                }
+            }
+
             console.log(fiveDay);
             console.log(response);
+            console.log(fiveDayWeather);
 
-        // document.querySelector(".city").innerHTML = "Weather Details: " + response.data.name;
+        // document.querySelector(".dayOne").innerHTML = "Temperature (F) " + response.data.main.temp;
         // document.querySelector(".wind").innerHTML = "Wind Speed: " + response.data.wind.speed;
         // document.querySelector(".humidity").innerHTML = "Humidity: " + response.data.main.humidity;
         // document.querySelector(".temp").innerHTML = "Temperature (F) " + response.data.main.temp;
 
-
+console.log(document.querySelector(".dayOne"));
 
         })
 
     })
 
 }
+fiveDayForecast()
 
 }
+  
 findWeather()
